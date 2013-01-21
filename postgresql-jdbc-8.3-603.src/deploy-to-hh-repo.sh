54abf4a0eacd596d78769d32559397f49d4e5291
mmfedotov <m.fedotov@hh.ru>
2013-01-21 17:26:05 +0400

    unix build scripts for 8.3-603

---
+++ postgresql-jdbc-8.3-603.src/deploy-to-hh-repo.sh
@@ -0,0 +1,9 @@
+echo ====================================================================
+echo Before you can use this script, you should copy the following jars
+echo to M2_HOME/lib
+echo - wagon-webdav-1.0-beta-2.jar
+echo - slide-webdavlib-2.1.jar
+echo - commons-httpclient-2.0.2.jar
+echo - commons-logging-1.0.4.jar
+echo ====================================================================
+mvn-hh deploy:deploy-file -DgroupId=postgresql -DartifactId=postgresql -Dpackaging=jar -Dversion=8.3-603.jdbc4-hh1 -Dsources=src.jar -Dfile=jars/postgresql.jar -Durl=dav:http://m2.hh.ru/content/repositories/releases -DrepositoryId=hh
---
+++ postgresql-jdbc-8.3-603.src/install-to-local-repo.sh
@@ -0,0 +1,1 @@
+mvn-hh install:install-file -DgroupId=postgresql -DartifactId=postgresql -Dpackaging=jar -Dversion=8.3-603.jdbc4-hh1 -Dsources=src.jar -Dfile=jars\postgresql.jar
